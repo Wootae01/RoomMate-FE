481fe278e4138b6296bb7b2910fdbb6e3a65d8ef
LeeHuiYun <dude1599@naver.com>
2025-02-28 15:46:46 +0900

    feat : 상대 프로필 정보 반환 api 요청 구현 feat : 사용자 프로필 정보 반환 api 요청 구현

---
+++ src/api/getinformation.js
@@ -0,0 +1,103 @@
+import axios from 'axios';
+
+/**
+ * 추천목록에서 상대방 '프로필 조회' 누를 시 상대 프로필 정보를 백엔드에서 반환 api 요청
+ * @param {number} friendId
+ * @return FriendData(basic+lifestyle+preferecne) : SignUpDTO Type
+ */
+
+export const getFriendInformation = async (friendId) => {
+    try {
+    const response = await axios.get(
+      `${process.env.EXPO_PUBLIC_API_BASE_URL}/members/${friendId}/information`, // membercontroller의 getFriendInformatino() 호출
+      {
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+
+    console.log('Friend Profile 반환에 대한 백엔드 응답:', response.data);
+    return response.data;
+  } catch (axiosError) {
+    console.error(
+      'Friend Profile 반환에 대한 백엔드 요청 실패:',
+      axiosError.response?.data || axiosError.message
+    );
+  }
+};
+
+/**
+ * 본인 Id를 통해 본인 프로필 정보를 백엔드에서 반환 api 요청
+ *
+ * @param {number} memberId
+ * @return Basic || LifeStyle || Preference
+ */
+
+// 사용자의 Basic_Profile만 가져올 경우 : EditMemberDTO Type
+export const getProfile = async (memberId) => {
+  try {
+    const response = await axios.get(
+      `${process.env.EXPO_PUBLIC_API_BASE_URL}/members/${memberId}/basic`, // membercontroller의 getProfile()로 전송
+      {
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+
+    console.log(
+      'Member basic profile 조회 요청에 대한 백엔드 응답:',
+      response.data
+    );
+    return response.data;
+
+  } catch (axiosError) {
+    console.error(
+      'Member basic profile 조회 요청에 대한 백엔드 요청 실패:',
+      axiosError.response?.data || axiosError.message
+    );
+  }
+};
+
+// 사용자의 LifeStyle만 가져올 경우 : LifeStyleDTO Type = Map<String, List<Long>>
+export const getLifeStyle = async (memberId) => {
+  try {
+    const response = await axios.get(
+      `${process.env.EXPO_PUBLIC_API_BASE_URL}/members/${memberId}/lifestyle`, // membercontroller의 getLifeStyle()로 전송
+      {
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+
+    console.log(
+      'Member LifeStyle 조회 요청에 대한 백엔드 응답:',
+      response.data
+    );
+    return response.data;
+  } catch (axiosError) {
+    console.error(
+      'Member LifeStyle 조회 요청에 대한 백엔드 요청 실패:',
+      axiosError.response?.data || axiosError.message
+    );
+  }
+};
+
+// 사용자의 Preference만 가져올 경우 : PreferenceDTO Type = Map<String, List<Long>>
+export const getPreference = async (memberId) => {
+    try {
+      const response = await axios.get(
+        `${process.env.EXPO_PUBLIC_API_BASE_URL}/members/${memberId}/preference`, // membercontroller의 getPreference()로 전송
+        {
+          headers: { 'Content-Type': 'application/json' },
+        }
+      );
+  
+      console.log(
+        'Member Preference 조회 요청에 대한 백엔드 응답:',
+        response.data
+      );
+      return response.data;
+    } catch (axiosError) {
+      console.error(
+        'Member Preference 조회 요청에 대한 백엔드 요청 실패:',
+        axiosError.response?.data || axiosError.message
+      );
+    }
+  };
