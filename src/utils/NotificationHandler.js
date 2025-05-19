ebaa6fd8e3ab2b88a4ea294eb253199973cdf2da
Wootae <comijoa2@naver.com>
2025-05-19 22:02:29 +0900

    fix: 처리된 알림 응답이 반복 호출되지 않도록 수정

--- src/utils/NotificationHandler.js
+++ src/utils/NotificationHandler.js
@@ -2,10 +2,10 @@
 import { useNavigation } from '@react-navigation/native';
 import * as Notifications from 'expo-notifications';
 import { useCallback, useContext, useEffect, useRef } from 'react';
+import ActiveChatRoomContext from '../contexts/ActiveChatRoomContext';
 import NotificationContext from '../contexts/NotificationContext';
 import UserContext from '../contexts/UserContext';
 import { ChatRoutes, MainRoutes, SignRoutes } from '../navigations/routes';
-import ActiveChatRoomContext from '../contexts/ActiveChatRoomContext';
 
 const NotificationHandler = () => {
   const { notification, setNotification } = useContext(NotificationContext);
@@ -15,9 +15,13 @@ const NotificationHandler = () => {
   const navigation = useNavigation();
   const notificationListener = useRef();
   const responseListener = useRef();
-
+  const hasHandledInitial = useRef(false); //알림 처리 유무 확인
   const checkInitialNotification = useCallback(async () => {
     const response = await Notifications.getLastNotificationResponseAsync();
+
+    if (hasHandledInitial.current) return; //이미 처리했으면 동작 안함
+    hasHandledInitial.current = true;
+
     if (response) {
       console.log('초기 알림 응답:', JSON.stringify(response));
       const content = response.notification.request.content;
